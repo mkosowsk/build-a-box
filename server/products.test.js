aa4fe018b01ca6f92dda29817c221ab52171d8c8
Mitch Kosowski <mitchkosowski@gmail.com>
2017-01-09 17:43:53 -0500

    added 2 user tests

--- server/products.test.js
+++ server/products.test.js
@@ -1,11 +1,11 @@
 const request = require('supertest-as-promised')
 const {expect} = require('chai')
 const db = require('APP/db')
-const User = require('APP/db/models/user')
+const Product = require('APP/db/models/user')
 const app = require('./start')
 
-describe('/api/users', () => {
-  describe('when not logged in', () => {
+describe('/api/products/', () => {
+  describe('if ', () => {
     it('GET /:id fails 401 (Unauthorized)', () =>
       request(app)
         .get(`/api/users/1`)
@@ -17,29 +17,9 @@ describe('/api/users', () => {
         .post('/api/users')
         .send({
           email: 'beth@secrets.org',
-          password: '12345',
+          password: '12345'
         })
         .expect(201)
-    )
-
-    it('POST fails to create a user when email is not given', () =>
-      request(app)
-        .post('/api/users')
-        .send({
-          email: '',
-          password: '12345',
-        })
-        .expect(400)
-    )
-
-    it('POST fails to create a user when email is not valid', () =>
-      request(app)
-        .post('/api/users')
-        .send({
-          email: 'invalidemail',
-          password: '12345',
-        })
-        .expect(400)
     )
 
     it('POST redirects to the user it just made', () =>
@@ -55,5 +35,4 @@ describe('/api/users', () => {
         }))        
     )
   })
-
 })
