cf1620ba6a07dc97425ac8438963a43528906c7a
Andres De Jesus <andom4r@gmail.com>
2017-01-09 14:30:12 -0500

    Good now

---
+++ build-a-box/server/api.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const db = require('APP/db')
+const api = module.exports = require('express').Router()
+
+api
+  .get('/heartbeat', (req, res) => res.send({ok: true,}))
+  .use('/auth', require('./auth'))
+  .use('/users', require('./users'))
+
+// Send along any errors
+api.use((err, req, res, next) => {
+  res.status(500).send(err)
+})
+
+// No routes matched? 404.
+api.use((req, res) => res.status(404).end())
