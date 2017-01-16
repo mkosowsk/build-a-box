e545a0967988e9ae7562c0b5a17c6f9898513d10
Andres De Jesus <andom4r@gmail.com>
2017-01-16 10:36:18 -0500

    minor changes

--- app/reducers/cart-reducer.js
+++ app/reducers/cart-reducer.js
@@ -18,6 +18,7 @@ export default function (state = initialCartState, action) {
       newState.list = action.cart;
       break;
 
+    
     default:
       return state;
 
--- server/api.js
+++ server/api.js
@@ -8,7 +8,7 @@ api
   .use('/auth', require('./auth'))
   .use('/users', require('./users'))
   .use('/products', require('./products'))
-  .use('/cart', require('./cart'))
+  
   
 
 // Send along any errors
